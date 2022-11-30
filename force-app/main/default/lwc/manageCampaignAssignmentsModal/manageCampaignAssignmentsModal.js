5601a2a628e474eb6d6722dc3bfb4a3e5ed32293
Miroslav Hristov <mhris@softserveinc.com>
2022-11-30 17:51:52 +0200

    Add custom field for relationship between UserCampaigns and Users

--- force-app/main/default/lwc/manageCampaignAssignmentsModal/manageCampaignAssignmentsModal.js
+++ force-app/main/default/lwc/manageCampaignAssignmentsModal/manageCampaignAssignmentsModal.js
@@ -2,6 +2,10 @@ import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
 
 export default class ManageCampaignAssignmentsModal extends LightningModal {
+    connectedCallback(){
+        
+    }
+
     handleClose(){
         console.log('close handler');
         this.close('canceled');
