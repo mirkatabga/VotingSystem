8de2ae6b8f58d4e222c61da6de3d53120d1bf572
Miroslav Hristov <mhris@softserveinc.com>
2022-12-01 17:55:15 +0200

    delete standard objects from repo

--- force-app/main/default/lwc/manageCampaignAssignmentsModal/manageCampaignAssignmentsModal.js
+++ force-app/main/default/lwc/manageCampaignAssignmentsModal/manageCampaignAssignmentsModal.js
@@ -1,15 +1,19 @@
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
+import getCampaignAssignments from '@salesforce/apex/CampaignController.getCampaignAssignments';
 
 export default class ManageCampaignAssignmentsModal extends LightningModal {
+    @api recordId;
+
     connectedCallback(){
-        
+        console.log(this.recordId);
     }
 
     handleClose(){
         console.log('close handler');
         this.close('canceled');
     }
+
     handleSave(){
         console.log('success handler');
         this.close('success');
--- force-app/main/default/lwc/manageCompaignAssignmenets/manageCompaignAssignmenets.js
+++ force-app/main/default/lwc/manageCompaignAssignmenets/manageCompaignAssignmenets.js
@@ -1,7 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import ManageCampaignAssignmentsModal from 'c/manageCampaignAssignmentsModal';
 
 export default class ManageCampaignAssignments extends LightningElement {
+    @api recordId;
+
     async handleClick() {
         await ManageCampaignAssignmentsModal.open({
             size: 'large'
